e89eb2e60732ec8b7f6b44146e9ae73bd9996a5b
Ruchir Bisht <ruchircr7@gmail.com>
2019-05-10 11:26:15 +0530

    commit 1

---
+++ SnakeGameTestProject/src/Main.java
@@ -0,0 +1,28 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+	public Main() {
+
+		JFrame frame = new JFrame();
+		GamePanel gamepanel = new GamePanel();
+		
+		frame.add(gamepanel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("SNAKE GAME by RUCHIR BISHT");
+		frame.setLocationRelativeTo(null);
+		
+		frame.pack();
+		frame.setVisible(true);
+
+
+	}
+
+	public static void main(String[] args) {
+
+		new Main();
+
+
+	}
+
+}
